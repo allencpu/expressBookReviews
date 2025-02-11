0158e3c3f61fcecf74520643b49b8e3b3cd14140
allencpu <allenproof@yahoo.com>
2025-02-10 22:39:03 -0800

    Update general.js
    sift books for data entries

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,41 +11,49 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/books',function (req, res) {
   //Write your code here
   res.send(books);
- // return res.status(300).json({message: "Yet to be implemented"});
+  r//eturn res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+
   //Write your code here
+  let book="";
+  let input="";
   const isbn = req.params.isbn;
-  res.send (books[isbn]);
- // return res.status(300).json({message: "Yet to be implemented"});
+  for (let x=0; x<9; x++){input = books[x+1];
+    if(input["isbn"] == isbn){book=x+1;
+    }
+}
+  res.send(books[book]);
+} else {
+  
+  return res.status(300).json({message: "Cannot locate that specific isbn"});
  });
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author',function (req, res) {
+  //Write your code here
+  let book="";
+  let input="";
   const author = req.params.author;
-  const bricks=books; 
-  let filtered_author = bricks.filter((book) => book.author === author);
-  if (filtered_author.length > 0) {
-
-
-  res.send(filtered_author);
-}else {
-  return res.status(300).json({message: "Cannot locate this TITLE"});
+  for (let x=0; x< 9 ; x++){input = books[x+1];
+    if (input["author"] == author){
+        book=x+1;
+    }
 }
+  res.send(books[book]);
+} else {
+  return res.status(300).json({message: "Cannot locate that author"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const title = req.params.title;
- // let selected_title = books.filter((book) => book.title === title); 
-  res.send(books.title);
-  //return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review

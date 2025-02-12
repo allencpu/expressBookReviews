93333540bb7cfce257e9f8d352f25b6ca0272060
allencpu <allenproof@yahoo.com>
2025-02-11 18:19:01 -0800

    Update general.js
    implemented function to RETRIEVE data segments

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,58 +4,52 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function getDetails( item , detail){
+    let book=0;
+    let input=""
+    for(let x=1; x<11; x++) {input = books[x];
+        if (input[item] == detail){
+            book=x;
+        }
+    }
+    if (book){return books[book];
+    }else {
+        return ("cannot locate this " + item);
+}}
+
 
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
-
 // Get the book list available in the shop
 public_users.get('/books',function (req, res) {
   //Write your code here
   res.send(books);
-  r//eturn res.status(300).json({message: "Yet to be implemented"});
+  //eturn res.status(300).json({message: "Yet to be implemented"});
 });
-
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-
   //Write your code here
-  let book="";
-  let input="";
-  const isbn = req.params.isbn;
-  for (let x=0; x<9; x++){input = books[x+1];
-    if(input["isbn"] == isbn){book=x+1;
-    }
-}
-  res.send(books[book]);
-} else {
-  
-  return res.status(300).json({message: "Cannot locate that specific isbn"});
+  const output = getDetails("ISBN",req.params.isbn);
+  res.send(output);
+  //return res.status(300).json({message: "Cannot locate that specific isbn"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  let book="";
-  let input="";
-  const author = req.params.author;
-  for (let x=0; x< 9 ; x++){input = books[x+1];
-    if (input["author"] == author){
-        book=x+1;
-    }
-}
-  res.send(books[book]);
-} else {
-  return res.status(300).json({message: "Cannot locate that author"});
+  const output = getDetails("author",req.params.author);
+  res.send(output);
+//return res.status(300).json({message: "Cannot locate that author"});
 });
-
-// Get all books based on title
+// Get books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const output = getDetails("title",req.params.title);
+  res.send(output);
+ // return res.status(300).json({message: "Yet to be implemented"});
 });
-
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here

24f9c1605cf17acb793a93607e28b360c85ba2a2
allencpu <allenproof@yahoo.com>
2025-02-21 00:34:57 -0800

    Update Index.js
    implementing /customer/login amnd jwt authentication

--- final_project/Index.js
+++ final_project/Index.js
@@ -4,45 +4,35 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+let users =[];
 const app = express();
+
 app.use(express.json());
 
 // Check user list
 const doesExist = (username) => {
-    if (users.includes( { name: {username}}))
-     {
-       return true;
-   } else {
-       return false;}
-   }
+     if (users.includes( { name: {username}}))
+      {
+        return true;
+    } else {
+        return false;}
+    }
+
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
+
 //Write the authenication mechanism here
-if (req.session.authorization){
-    let token = req.session.authorization['accessToken'];
-    
-    // check JWT
-    jwt.verify(token,"access",(err,user) => {
-        if (!err) {
-            req.user = user;
-            next();
-        } else {
-            return res.status(403).json({message: "not Valid User"});
-        }
-    });
-} else {
-    return res.status(403).json({message: "User must Log in!"});
-}
+
 });
 // Customer Login
 app.get('/users',function (req, res) {
     //Write your code here
     res.send(users);
-    //return res.status(300).json({message: "Yet to be implemented"});
+    //eturn res.status(300).json({message: "Yet to be implemented"});
   });
-  app.post("/customer/login", (req,res) => {
+app.post("/customer/login", (req,res) => {
     username = req.body.username;
     password = req.body.password;
     // Verify Login info
@@ -50,8 +40,9 @@ app.get('/users',function (req, res) {
         return res.status(403).json({message: "not Valid Login"});
     }
 })
-          // Register new User
-          app.post("/register", (req,res) => {
+
+// Register new User
+        app.post("/register", (req,res) => {
             const username = req.body.username;
             const password = req.body.password;
             // check Login info
@@ -66,9 +57,7 @@ app.get('/users',function (req, res) {
                       
                 }
             }
-       return res.status(404).json({message: "Cannot register-- invalid User"});
-     });
-    
+        })
  
 const PORT =5000;
 

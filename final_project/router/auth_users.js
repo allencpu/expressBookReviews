2c164598afcfb670ef6c78dcdc499340dbd3e5de
allencpu <allenproof@yahoo.com>
2025-02-24 15:40:18 -0800

    Update auth_users.js
    add authorization privileges for registered customers

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,51 +7,56 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
-}
 
-const authenticatedUser = (username,password)=>{
- // Validate users information matches the registered user
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
+    if (users.includes( { name: {username}}))
+     {
+       return true;
+   } else {
+       return false;}
+   }
 
+
+
+const authenticatedUser = (username,password)=>{ //returns boolean
+//write code to check if username and password match the one we have in records.
+}
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-   username = req.body.username;
-    password = req.body.password;
-    // Verify Login info
-    if (!username || !password){
-        return res.status(403).json({message: "not Valid Login"});
-    }
-    
-         
-    
-  //Write your code here
-   if(authenticatedUser(username,password)){
-     
-        // create tokem
-        let accessToken = jwt.sign({
-            data: password
-        },'access',{expiresIn:  60*60});
+//Write your code here
+username = req.body.username;
+password = req.body.password;
+// Verify Login info
+if (!username || !password){
+    return res.status(403).json({message: "not Valid Login"});
+}
 
-            req.session.authorization = {
-                accessToken, username}
-                return res.status(200).send("Customer "+ username + " now Logged in");
-            } else { 
-                return res.status(208).json({message: "Customer Not Logged In"});
-            }
-            
-        });
-  
+const authenticatedUser = (username,password) => {
+    if (users.includes( { name: {username}}) &&
+    {username: {password} == password}
+)
+  {
+    return false;
+} else {
+    //users.push({"username":username,"password":password});
+    return true;}
+
+}
+if(authenticatedUser(username,password)){
+    // create tokem
+    let accessToken = jwt.sign({
+        data: password
+    },'access',{expiresIn:  60*60});
+
+        req.session.authorization = {
+            accessToken, username}
+            return res.status(200).send("Customer "+ username + " now Logged in");
+        } else { 
+            return res.status(208).json({message: "Customer Not Logged In"});
+        }
+        
+
+});
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {

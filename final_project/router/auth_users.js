61adff9e703be1646c9857d07ecb06b077d56b3c
allencpu <allenproof@yahoo.com>
2025-02-27 15:11:14 -0800

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,6 +5,7 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+let titles = books;
 
 const isValid = (username)=>{ 
 //returns boolean
@@ -19,15 +20,29 @@ const isValid = (username)=>{
 
    function findReviews (item, detail) {
     let book =0; let input ="";
-    for(let x=1; x<11;x++) {input = books[x];
+    for(let x=1; x<11;x++) {input = titles[x];
     if (input.ISBN == detail ){book=x;
     }
     if (book > 0){
-        return books[book].reviews;
+        return titles[book].reviews;
     }else {
         return ("Cannot locate this " + detail);
     }
 }}
+function findDetails( item , detail){
+    let book=0;
+    let input=""
+    for(let x=1; x<11; x++) {input = titles[x];
+        if (input[item] == detail){
+            book=x;
+        }
+    }
+    
+    if (book){return book;
+    }else {
+        return ("cannot locate this " + item);
+}}
+
 
 
 //only registered users can login
@@ -67,18 +82,39 @@ if(authenticatedUser(username,password)){
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    let  reviews = req.query.reviews;
-    let review =req.body.review;
+    
+    let review =(req.body.reviews).review;
     const isbn =req.params.isbn;
     //const filtered_book =getDetails("ISBN",isbn);
-    let oldReview = findReviews("reviews",isbn);
-    oldReview.push({"username":username,"review":review})
+    let oldBook = findDetails("ISBN",isbn);
+    let author = req.query.author;
+    if (author) {newBook.author =author};
+
+    let title = req.query.title;
+    if (title) {newBook.title = title};
+    let ISBN = req.query.ISBN;
+    if (ISBN) { newBook.ISBN =ISBN};
+    let reviews =req.query.reviews;
+
+    if (reviews) {
+        reviews.push({"username":username,"review":review});
+         newBook.reviews =reviews};
+
+    //oldReview.push({"username":username,"review":review})
+   // let newReview = findReviews("reviews",isbn)
       
-        //books.push(filtered_book);
-        return res.status(200).send("Customer "+ username + "'s review is finished"+ review);
-});
+       // titles.push(...oldReview);
+        //return res.status(200).send("Customer "+ username + "'s review  "+ titles);
+    res.send(review);
+    });
 regd_users.delete("/auth/review/:isbn", (req, res) => {
 })
+//  Get book review
+regd_users.get('/review/:isbn',function (req, res) {
+    const output =findReviews("reviews",req.params.isbn);
+    res.send(output);
+    
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;

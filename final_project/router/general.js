d99e09db8e0192e819342601225a365fe1d038dc
allencpu <allenproof@yahoo.com>
2025-03-03 07:41:49 -0800

    Update general.js
    inserting promise call version of books by title listings

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,10 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const fs = require('fs');
+
+let fileData = "./bookInventory.json";
+
 function getDetails( item , detail){
     let book=0;
     let input=""
@@ -31,44 +35,55 @@ function getReviews (item, detail){
 }}
 
 
+function readFile(fileName){
+    let data = fs.readFileSync(fileName);
+    return data;
+}
 
-public_users.post("/register", (req,res) => {
+// Get the book list available in the shop
+public_users.get('/prbooks',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Display information
+console.log('Getting information');
+let result = (readFile(fileData)).toString();
+res.send(result);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
+
 // Get the book list available in the shop
 public_users.get('/books',function (req, res) {
-  //Write your code here
-  res.send(books);
-  //eturn res.status(300).json({message: "Yet to be implemented"});
-});
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const output = getDetails("ISBN",req.params.isbn);
-  res.send(output);
+    //Write your code here
+    res.send(books);
+    //eturn res.status(300).json({message: "Yet to be implemented"});
+  });
+  // Get book details based on ISBN
+  public_users.get('/isbn/:isbn',function (req, res) {
+    //Write your code here
+    const output = getDetails("ISBN",req.params.isbn);
+    res.send(output);
+    
+   });
+    
+  // Get book details based on author
+  public_users.get('/author/:author',function (req, res) {
+    //Write your code here
+    const output = getDetails("author",req.params.author);
+    res.send(output);
   
- });
+  });
+  // Get books based on title
+  public_users.get('/title/:title',function (req, res) {
+    //Write your code here
+    const output = getDetails("title",req.params.title);
+    res.send(output);
+   
+  });
+  //  Get book review
+  public_users.get('/review/:isbn',function (req, res) {
+    const output =getReviews("reviews",req.params.isbn);
+    res.send(output);
+    
+  });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const output = getDetails("author",req.params.author);
-  res.send(output);
+  module.exports.general = public_users;
 
-});
-// Get books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const output = getDetails("title",req.params.title);
-  res.send(output);
- 
-});
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  const output =getReviews("reviews",req.params.isbn);
-  res.send(output);
-  
-});
-
-module.exports.general = public_users;

d450541b58dbbe422979430a61ce357646b511f5
allencpu <allenproof@yahoo.com>
2025-03-05 20:27:06 -0800

    Update general.js
    general.js extended /pr version for promise users

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,9 +4,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const fs = require('fs');
+//const fs = require('fs');
 
-let fileData = "booksInventory.json";
+//let fileData = "booksInventory.json";
 
 function getDetails( item , detail){
     let book=13;
@@ -16,8 +16,7 @@ function getDetails( item , detail){
             book=x;
         }
     }
-    
-    if (book<13){return books[book];
+    if (book<=13){return books[book];
     }else {
         return ("cannot locate this " + item);
 }}
@@ -43,14 +42,37 @@ function readFile(fileName){
 }
 
 // Get the book list available in the shop
-public_users.get('/prbooks',function (req, res) {
+public_users.get('/pr/books',function (req, res) {
   //Write your code here
+  const methCall = new Promise((resolve, reject) => {
+    // Prompting the user to input the filename
+     try {
+    const result = books;   
+    resolve(result); 
+    } catch (err) {
+        reject(err);
+    }});
   // Display information
-console.log('Getting information');
-let result = (readFile(fileData)).toString();
-res.send(result);
-
+methCall.then((result) =>
+res.send(result),
+(err)=> res.send("err"));
 });
+  // Get books based on title using promise method
+  public_users.get('pr/title/:title',function (req, res) {
+    //Write your code here
+    let titleA= (req.params.title).toString();
+const methCall = new Promise((resolve, reject)=> {
+    try {
+        const result = getDetails("title",req.params.title.toString());
+        resolve(result);
+    }catch (err) {
+        reject(err);
+    }});
+methCall.then((result) =>
+res.send(result),
+(err) => res.send("cannot locate the title ***"+titleA+"***"));
+  });
+
 
 // Get the book list available in the shop
 public_users.get('/books',function (req, res) {
@@ -73,18 +95,18 @@ public_users.get('/books',function (req, res) {
     res.send(output);
   });
 
-  // Get books based on title
+
   public_users.get('/title/:title',function (req, res) {
     //Write your code here
     const output = getDetails("title",req.params.title);
     res.send(output);
-   
-  });
+  
+});
   //  Get book review
   public_users.get('/review/:isbn',function (req, res) {
     const output =getReviews("reviews",req.params.isbn);
     res.send(output);
     
-  });
+});
   
   module.exports.general = public_users;
